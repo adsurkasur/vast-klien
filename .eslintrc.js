0012e220cd865cf5bc8b8e197d8d3df1b03e04eb
adsurkasur <adsurkasur.dev@gmail.com>
2025-08-24 16:01:30 +0700

    feat: update ESLint configuration and add TypeScript support

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,7 +1,17 @@
-module.exports = {
-  extends: ["next", "next/core-web-vitals", "plugin:@typescript-eslint/recommended"],
+export default {
+  root: true,
+  extends: [
+    "next",
+    "next/core-web-vitals",
+    "plugin:@typescript-eslint/recommended"
+  ],
   plugins: ["@next/next"],
   rules: {
     // Add any custom rules here
+    "@typescript-eslint/no-unused-vars": "off",
+    "react-refresh/only-export-components": [
+      "warn",
+      { allowConstantExport: true }
+    ]
   },
 };
