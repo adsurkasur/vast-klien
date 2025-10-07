d3ef2eabaa49c9be24453f5519b74bf21f363624
adsurkasur <adsurkasur.dev@gmail.com>
2025-10-07 20:35:40 +0700

    Enhance Google Analytics integration with debug logging support

--- src/components/GoogleAnalytics.tsx
+++ src/components/GoogleAnalytics.tsx
@@ -1,7 +1,11 @@
 "use client";
 import Script from "next/script";
 
+
 const GA_MEASUREMENT_ID = "G-SSXNMH6N0M";
+// Enable debug logging if NEXT_PUBLIC_GA_DEBUG is set to 'true'
+const GA_DEBUG = process.env.NEXT_PUBLIC_GA_DEBUG === 'true';
+
 
 export default function GoogleAnalytics() {
   // Only render in production
@@ -12,13 +16,25 @@ export default function GoogleAnalytics() {
         src={`https://www.googletagmanager.com/gtag/js?id=${GA_MEASUREMENT_ID}`}
         strategy="afterInteractive"
         async
+        onLoad={GA_DEBUG ? () => console.log('[GA DEBUG] gtag.js script loaded') : undefined}
       />
       <Script id="gtag-init" strategy="afterInteractive">
         {`
           window.dataLayer = window.dataLayer || [];
-          function gtag(){dataLayer.push(arguments);}
+          function gtag(){
+            dataLayer.push(arguments);
+            ${GA_DEBUG ? "console.log('[GA DEBUG] gtag call:', arguments);" : ""}
+          }
           gtag('js', new Date());
           gtag('config', '${GA_MEASUREMENT_ID}');
+          ${GA_DEBUG ? "console.log('[GA DEBUG] gtag config initialized');" : ""}
+          // Monkey-patch window.gtag to log all calls if debug is enabled
+          if (${GA_DEBUG}) {
+            window.gtag = function() {
+              console.log('[GA DEBUG] gtag event:', arguments);
+              dataLayer.push(arguments);
+            }
+          }
         `}
       </Script>
     </>
